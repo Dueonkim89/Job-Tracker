7aa249193a0d27e42a6c8d50396952b1d688e539
Jon Baird <jbaird29@gmail.com>
2022-04-25 17:03:11 -0400

    clean and repopulate DB before each test run

--- src/server/config/jestGlobalSetup.js
+++ src/server/config/jestGlobalSetup.js
@@ -1,3 +1,5 @@
-module.exports = function (globalConfig, projectConfig) {
+module.exports = async function (globalConfig, projectConfig) {
     require("dotenv").config();
+    const setUpDB = require("./setupDB");
+    await setUpDB();
 };
