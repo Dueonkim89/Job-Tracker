fa4d4590eeb36ccda05aaf47c581e1e65a150690
Timothy Pham <phamtimo@oregonstate.edu>
2022-04-25 20:00:45 -0500

    changed login page to function component and added login feature

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,31 +1,42 @@
-import * as React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Form, Button, Nav } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 import {validStringData, validPassword} from '../utils/formValidation';
 
-// declare prop & state types for Login component
-type MyState = { 
-    userName: string,
-    password: string,
-    userNameValid: boolean,
-    passwordValid: boolean
-};
+function Login() {
+    let navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [usernameValid, setUsernameValid] = useState(true);
+    const [passwordValid, setPasswordValid] = useState(true);
+    // auth token
+    const [loggedIn, setLoggedIn] = useState(localStorage.getItem("token"));
 
-class Login extends React.Component<{}, MyState> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            userName: '',
-            password: '',
-            userNameValid: true,
-            passwordValid: true
-        };
-        this.enterUserName = this.enterUserName.bind(this);
-        this.enterPassword = this.enterPassword.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    // checks if the token in the local storage matches the one created when the user logged in. 
+    // if it is the user is directed to protected page
+    useEffect(() => {
+        if (loggedIn) {
+            axios.get("http://localhost:3001/users/login", {
+            headers: {
+                Authorization: loggedIn,
+            }
+            }).then(res => {
+                console.log(res);
+                navigate('/protected');
+            }).catch(err => {
+                console.log(err);
+            })
+        } else {
+            navigate('/login')
+        }
 
-    generateLeftLoginPanel(argument: void) : JSX.Element {
+        const token = localStorage.getItem('token');
+        
+        
+    }, [])
+
+    const generateLeftLoginPanel = (argument: void) : JSX.Element => {
         return (                        
         <Container className="login-text-box">
             <br />
@@ -36,51 +47,46 @@ class Login extends React.Component<{}, MyState> {
         );
     }
 
-    enterUserName(event: any) {
-        this.setState({userName: event.target.value});
-    }
-
-    enterPassword(event: any) {
-        this.setState({password: event.target.value});
-    }
-
-    handleSubmit(event: any) {
+    // Once the user clicks submit and the login username and password have been authenticated 
+    // the user will be redirected to the protected page
+    const handleSubmit = (event: any) => {
         event.preventDefault();
         
         // See if all the form field has data and a valid strong password
-        this.setState({ 
-            userNameValid: validStringData(this.state.userName),
-            passwordValid: validPassword(this.state.password)
-        });
-
+        setUsernameValid(validStringData(username));
+        //setPasswordValid(validPassword(password));
+        
         // only make GET request when all the form field is valid
-        if (validStringData(this.state.userName) && validPassword(this.state.password)) {
+        if (validStringData(username) && validPassword(password)) {
             console.log("To make a GET request");
-
-            // CONDITIONAL route to user dashboard, pass in props received from server
-            // https://stackoverflow.com/questions/45805930/react-router-redirect-conditional
-
-                // update redux store before routing, so appbar is updated. 
+            console.log(username, password);
+            axios.post("http://localhost:3001/users/login", { username, password }).then(user => {
+                console.log(user);
+                localStorage.setItem('token', user.data.token);
+                navigate('/protected');
+            }).catch(err => {
+                console.log(err);
+            })
         }
             
     }
 
-    generateRightLoginPanel(argument: void) : JSX.Element {
+    const generateRightLoginPanel = (argument: void) : JSX.Element => {
         return (
             <Container className="login-form-field">
-                {this.generateLoginForm()}
+                {generateLoginForm()}
                 <div className='create-account'>
-                    {this.generateCreateAnAccount()}
+                    {generateCreateAnAccount()}
                 </div>
                 
             </Container>   
         );
     }
 
-    generateLoginForm(argument: void) : JSX.Element {
+    const generateLoginForm = (argument: void) : JSX.Element => {
         const invalidStyle = '3px solid red';
         return (
-            <Form onSubmit={this.handleSubmit}>
+            <Form onSubmit={handleSubmit}>
                 <br />
                 <Form.Group className="mb-3">
                     <Form.Text style={{color: 'red'}} id="requiredAsterick">* </Form.Text>
@@ -89,17 +95,18 @@ class Login extends React.Component<{}, MyState> {
                     </Form.Text>
                 </Form.Group>
                 <Form.Group>
-                    {this.renderInvalidInput()}
+                    {renderInvalidInput()}
                 </Form.Group>
                 <Form.Group className="mb-3">
-                    <Form.Label htmlFor="userName" style={{fontWeight: 'bold'}}>Username</Form.Label>
+                    <Form.Label htmlFor="username" style={{fontWeight: 'bold'}}>username</Form.Label>
                     <Form.Text style={{color: 'red'}} id="requiredAsterick"> *</Form.Text>
-                    <Form.Control style={{ border: !this.state.userNameValid ? invalidStyle: ''}} id="userName" type="text" value={this.state.userName} onChange={this.enterUserName} placeholder="Enter username" />
+                    <Form.Control style={{ border: !usernameValid ? invalidStyle: ''}} id="username" type="text" value={username} onChange={event => setUsername(event.target.value)} placeholder="Enter username" />
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label htmlFor="password" style={{fontWeight: 'bold'}}>Password</Form.Label>
                     <Form.Text style={{color: 'red'}} id="requiredAsterick"> *</Form.Text>
-                    <Form.Control style={{ border: !this.state.passwordValid ? invalidStyle: ''}} id="password" type="password" value={this.state.password} onChange={this.enterPassword} placeholder="Enter password" />
+                    {/* re-add style={{ border: !passwordValid ? invalidStyle: ''}}  */}
+                    <Form.Control id="password" type="password" value={password} onChange={event => setPassword(event.target.value)} placeholder="Enter password" />
                 </Form.Group>
                 <Form.Group>
                     <Link to="../registration">Forgot password?</Link>
@@ -111,32 +118,30 @@ class Login extends React.Component<{}, MyState> {
         );  
     }
 
-    renderInvalidInput(argument: void) {
-        if (!this.state.userNameValid || !this.state.passwordValid) {
+    const renderInvalidInput = (argument: void) => {
+        if (!usernameValid || !passwordValid) {
             return <Form.Text style={{color: 'red'}} id="requiredAsterick">Your username or password is incorrect.</Form.Text>
         };
     }
 
-    generateCreateAnAccount(argument: void) : JSX.Element {
+    const generateCreateAnAccount = (argument: void) : JSX.Element => {
         return (
             <Link to="../registration" className="create-account"><br />Create an account</Link>
         );
     }
 
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <Col>
-                        {this.generateLeftLoginPanel()}
-                    </Col>
-                    <Col>
-                        {this.generateRightLoginPanel()}
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    {generateLeftLoginPanel()}
+                </Col>
+                <Col>
+                    {generateRightLoginPanel()}
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default Login;
