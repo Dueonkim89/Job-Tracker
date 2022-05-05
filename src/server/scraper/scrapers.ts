b849b8156cccaddb4f4c6f03ace2eb65acba9f22
Jon Baird <jbaird29@gmail.com>
2022-05-05 19:32:24 -0400

    change from fetch to axios

--- src/server/scraper/scrapers.ts
+++ src/server/scraper/scrapers.ts
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import axios from "axios";
 import { titleCase } from "title-case";
 
 interface ScrapedAppInfo {
@@ -21,11 +21,11 @@ export async function greenhouse(url: URL) {
     const company = pathparts[1];
     const postID = pathparts[3];
     const apiURL = `https://boards-api.greenhouse.io/v1/boards/${company}/jobs/${postID}`;
-    const response = await fetch(apiURL);
-    if (!response.ok) {
+    const response = await axios.get(apiURL, { responseType: "json" });
+    if (response.status < 200 || response.status >= 300) {
         throw Error(`Invalid response code: ${response.status} ${response.statusText}`);
     }
-    const data: any = await response.json();
+    const { data } = response;
     const location = data?.location?.name;
     const title = data?.title;
     return makeAppInfo({ company: titleCase(company), location, title });
