b399b0f14523eb0ee18f2ce6442fecc4e2e87cc6
unknown <dueonkim89@Yahoo.com>
2022-05-04 18:33:42 -0700

    helper function to interact with local storage

--- src/client/utils/helper.js
+++ src/client/utils/helper.js
@@ -15,6 +15,17 @@ function formatPhoneNumber(phoneNumber) {
     return formattedString.join('');
 }
 
+function checkIfTokenExists() {
+    if (localStorage.getItem("user")) {
+        return true;
+    }
+    return false;
+}
+
+function storeUserInfoIntoLocalStorage(data) {
+    localStorage.setItem("user", JSON.stringify(data));
+}
+
 function getServerURL(nodeEnv) {
     /*  INPUT: node_env of application
         OUTPUT: url of server */
@@ -23,9 +34,8 @@ function getServerURL(nodeEnv) {
 
 function registerNewUser(userField) {
     return axios.post("/api/users", userField).then(function (response) {
-        // send promise.resolve
-        // to send token and other user info
-        return Promise.resolve({"success": true});
+        // send promise.resolve. Include token, userId and success value
+        return Promise.resolve({"success": response.data.success, "token": response.data.token, "userID": response.data.userID});
     }).catch(function (error) {
         if (error.response && error.response.data.reason === "duplicate") {
             return Promise.reject({"success": false, "duplicate": true});
@@ -35,4 +45,4 @@ function registerNewUser(userField) {
 
 }
 
-export {formatPhoneNumber, getServerURL, registerNewUser}
+export {formatPhoneNumber, getServerURL, registerNewUser, checkIfTokenExists, storeUserInfoIntoLocalStorage}
