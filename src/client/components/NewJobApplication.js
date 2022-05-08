a74e4485a54c9c8c1f7b40e70a4ae958402ecca6
dueonkim89 <dueonkim89@yahoo.com>
2022-05-08 13:22:15 -0700

    controlled components, dynamically show job required skill when user adds to application

--- src/client/components/NewJobApplication.js
+++ src/client/components/NewJobApplication.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Container, Row, Form, Button, Col } from 'react-bootstrap';
 import {getListOfAllCompanies, getUserToken, getAllSkills} from '../utils/helper';
+import {validStringData} from '../utils/formValidation';
 import {UserLoggedInContext} from "../context/UserLoggedInStatus";
 import { Navigate, useNavigate, Link,  useLocation } from "react-router-dom"
 
@@ -9,17 +10,28 @@ const labelFontSize = "1.2rem";
 
 class NewJobApplication extends React.Component {
     constructor(props) {
-        console.log(props);
+        // CAN get PROPS from constructor
         super(props);
         this.state = {
            companyList: [],
-           skillList: [],
+           skillListFromServer: [],
            applicationSkillList: [],
-           companyName: ""
+           companyName: "",
+           title: "",
+           location: "",
+           url: "",
+           skill: "",
+           status: ""
         };
 
         // this.enterFirstName = this.enterFirstName.bind(this);
         this.pickCompany = this.pickCompany.bind(this);
+        this.enterTitle = this.enterTitle.bind(this);
+        this.enterLocation = this.enterLocation.bind(this);
+        this.enterURL = this.enterURL.bind(this);
+        this.enterSkill = this.enterSkill.bind(this);
+        this.pickAppStatus = this.pickAppStatus.bind(this);
+        this.addSkillToRequiredList = this.addSkillToRequiredList.bind(this);
     }
 
     globalLoggedInState = undefined;
@@ -32,7 +44,39 @@ class NewJobApplication extends React.Component {
 
     pickCompany(event) {
         this.setState({companyName: event.target.value});
-      }
+    }
+
+    enterTitle(event)  {
+        this.setState({title: event.target.value});
+    }
+
+    enterLocation(event) {
+        this.setState({location: event.target.value});
+    }
+
+    enterURL(event) {
+        this.setState({url: event.target.value});
+    }
+
+    enterSkill(event) {
+        this.setState({skill: event.target.value});
+    }
+
+    pickAppStatus(event) {
+        this.setState({status: event.target.value});
+    }
+
+    addSkillToRequiredList() {
+        // update state of applicationSkillList
+        // reset state of skill to empty string
+        const {skill} = this.state;
+        if (validStringData(skill)) {
+            this.setState(prevState => ({
+                applicationSkillList: [...prevState.applicationSkillList, skill]
+            }));
+            this.setState({skill: ""});
+        }
+    }
 
     componentDidMount() {
         // make call to server and get all company list and skills only if user logged in
@@ -54,7 +98,7 @@ class NewJobApplication extends React.Component {
 
             getAllSkills().then(
                 (result) => {
-                    this.setState({skillList: result});
+                    this.setState({skillListFromServer: result});
                 }
             ).catch((error) => {
                 return;
@@ -84,6 +128,7 @@ class NewJobApplication extends React.Component {
                     <Form.Control id="url" type="text" placeholder="Enter url" />
                 </Form.Group>
                 {this.generateApplicationSkills()}
+                {this.displayCurrentJobRequiredSkills()}
                 <Form.Group style={{padding: formPadding}} >
                     <Form.Label htmlFor="status" style={{fontWeight: 'bold', fontSize: labelFontSize}}>Application status</Form.Label>
                     {this.createStatusDropDrownMenu()}
@@ -106,8 +151,8 @@ class NewJobApplication extends React.Component {
 
     createStatusDropDrownMenu() {
         return (
-            <Form.Select aria-label="Choose application status from dropdown menu" id="status">
-                <option>Pick application status</option>
+            <Form.Select value={this.state.status} onChange={this.pickAppStatus} aria-label="Choose application status from dropdown menu" id="status">
+                <option value="">Pick application status</option>
                 <option value="applied">Applied</option>
                 <option value="online_assessment">Online Assessment</option>
                 <option value="phone_interview">Phone Interview</option>
@@ -142,29 +187,39 @@ class NewJobApplication extends React.Component {
             <Row style={{padding: formPadding}}>
                 <Form.Group as={Col}>
                     <Form.Label htmlFor="jobSkills" style={{fontWeight: 'bold', fontSize: labelFontSize}}>Job skills</Form.Label>
-                    <Form.Control list="skills" id="jobSkills" type="text" placeholder="Enter job skills" name="jobSkills"/>
+                    <Form.Control list="skills" id="jobSkills" type="text" placeholder="Enter job skills" name="jobSkills" value={this.state.skill} onChange={this.enterSkill} />
                     <datalist id="skills">
                         {this.getSkills()}
                     </datalist>
                 </Form.Group>
                 <Form.Group as={Col} style={{position: "relative", marginLeft: "2.5rem"}}>      
                         {/*Position the button at bottomn left corner of parent*/}
-                    <Button style={{position: "absolute", bottom: "0px", "left": "0px"}} variant="primary">Add skill</Button>
+                    <Button style={{position: "absolute", bottom: "0px", "left": "0px"}} onClick={this.addSkillToRequiredList} variant="primary">Add skill</Button>
                 </Form.Group>
             </Row>
         );
     }
 
+    displayCurrentJobRequiredSkills() {
+        // display current required skills as string
+        if (this.state.applicationSkillList.length > 0) {
+            return (
+                <p style={{paddingLeft: formPadding, marginBottom: "0rem"}}>{this.state.applicationSkillList.join(', ')}</p>
+            );
+        }
+    }
+
     getSkills() {
         // render dynamically from state
-        const {skillList} = this.state;
+        const {skillListFromServer} = this.state;
         return (
-            skillList.map((skill) => 
+            skillListFromServer.map((skill) => 
              <option key={skill.skillID} value={skill.name}>{skill.name}</option>
         ));
     }
 
     render() {
+        console.log(this.state.applicationSkillList);
         // redirect to login if user is not logged in
         const ApplicationFormBorder = "3px solid #0a2a66";
         return (
