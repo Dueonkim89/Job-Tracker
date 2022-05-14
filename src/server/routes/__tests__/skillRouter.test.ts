3bf1afeb70dff142b66dca3ecefc33e30544a856
Jon Baird <jbaird29@gmail.com>
2022-05-14 15:15:46 -0400

    add authorization checking to skill endpoints

--- src/server/routes/__tests__/skillRouter.test.ts
+++ src/server/routes/__tests__/skillRouter.test.ts
@@ -4,12 +4,14 @@ import request from "supertest";
 import { app } from "../../index";
 
 let server: http.Server;
-let token: string;
+let token1: string;
+let token2: string;
+let token3: string;
 
-const logInGetToken = async () => {
+const logInGetToken = async (username: string) => {
     const body = {
-        username: "ally1",
-        password: "ally1",
+        username: username,
+        password: username,
     };
     const result = await request(server).post("/api/users/login").send(body);
     if (!result.body.success) {
@@ -20,7 +22,12 @@ const logInGetToken = async () => {
 
 beforeAll((done) => {
     server = app.listen(async () => {
-        token = await logInGetToken();
+        const token1_promise = logInGetToken("ally1");
+        const token2_promise = logInGetToken("bryson2");
+        const token3_promise = logInGetToken("carlos3");
+        token1 = await token1_promise;
+        token2 = await token2_promise;
+        token3 = await token3_promise;
         return done();
     });
 });
@@ -49,10 +56,15 @@ test("[Valid] Getting all user skills", async () => {
         { userID: 1, name: "Java", skillID: 1, rating: 5 },
         { userID: 1, name: "C++", skillID: 2, rating: 3 },
     ];
-    const result = await request(server).get("/api/skills/user?userID=1").set("Authorization", token).send();
+    const result = await request(server).get("/api/skills/user?userID=1").set("Authorization", token1).send();
     expect(result.statusCode).toEqual(200);
     expect(result.body).toContainEqual(expected[0]);
     expect(result.body).toContainEqual(expected[1]);
+});
+
+test("[Invalid] Trying to get another user skills", async () => {
+    const result = await request(server).get("/api/skills/user?userID=1").set("Authorization", token3).send();
+    expect(result.statusCode).toEqual(401);
 });
 
 test("[Valid] Getting all application skills", async () => {
@@ -62,7 +74,7 @@ test("[Valid] Getting all application skills", async () => {
     ];
     const result = await request(server)
         .get("/api/skills/application?applicationID=1")
-        .set("Authorization", token)
+        .set("Authorization", token1)
         .send();
     expect(result.statusCode).toEqual(200);
     expect(result.body).toContainEqual(expected[0]);
@@ -79,21 +91,33 @@ test("[Valid] Adding a new skill", async () => {
 
 test("[Valid] Adding a new skill to a user", async () => {
     const payload = { userID: 1, skillID: 7, name: "Python", rating: 5 };
-    const result = await request(server).post("/api/skills/user").set("Authorization", token).send(payload);
+    const result = await request(server).post("/api/skills/user").set("Authorization", token1).send(payload);
     expect(result.statusCode).toEqual(201);
     expect(result.body.success).toEqual(true);
+});
+
+test("[Invalid] Attempting to add a skill to another user", async () => {
+    const payload = { userID: 1, skillID: 7, name: "Python", rating: 5 };
+    const result = await request(server).post("/api/skills/user").set("Authorization", token3).send(payload);
+    expect(result.statusCode).toEqual(401);
 });
 
 test("[Valid] Adding a new skill to an application", async () => {
     const payload = { applicationID: 1, skillID: 7, name: "Python" };
-    const result = await request(server).post("/api/skills/application").set("Authorization", token).send(payload);
+    const result = await request(server).post("/api/skills/application").set("Authorization", token1).send(payload);
     expect(result.statusCode).toEqual(201);
     expect(result.body.success).toEqual(true);
 });
 
+test("[Invalid] Attempting to add a new skill to another user's application", async () => {
+    const payload = { applicationID: 1, skillID: 7, name: "Python" };
+    const result = await request(server).post("/api/skills/application").set("Authorization", token3).send(payload);
+    expect(result.statusCode).toEqual(401);
+});
+
 test("[Valid] Updating a user's skill rating", async () => {
     const payload = { userID: 2, skillID: 5, rating: 4 };
-    const result = await request(server).patch("/api/skills/user").set("Authorization", token).send(payload);
+    const result = await request(server).patch("/api/skills/user").set("Authorization", token2).send(payload);
     expect(result.statusCode).toEqual(200);
     expect(result.body.success).toEqual(true);
 });
--- src/server/routes/skillRouter.ts
+++ src/server/routes/skillRouter.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import passport from "passport";
+import appModel from "../models/applicationModel";
 import skillModel from "../models/skillModel";
 import { ApplicationSkillFields, Skill, UserSkillFields } from "../types/skill";
-import { validateAndParseStringID, ValidationError } from "../types/validators";
+import { AuthError, validateAndParseStringID, validateAuthorization, ValidationError } from "../types/validators";
 const router = express.Router();
 
 /**
@@ -28,14 +29,16 @@ router.get("/", async function (req, res, next) {
  * or HTTP 400 and JSON of {success: false, message: "reason for error"}
  */
 router.get("/user", passport.authenticate("jwt", { session: false }), async function (req, res, next) {
-    // TODO - auth verification
+    const requestorID = req.user?.userID as number;
     const { userID } = req.query;
     try {
         const parsedUserID = validateAndParseStringID(userID);
+        validateAuthorization(requestorID, parsedUserID);
         const rows = await skillModel.getUserSkills(parsedUserID);
         res.status(200).send(rows);
     } catch (err) {
         if (err instanceof ValidationError) return res.status(400).json({ success: false, message: err.message });
+        if (err instanceof AuthError) return res.status(401).json({ success: false, message: err.message });
         console.error(`Error in getting user skills:`);
         console.error({ userID });
         next(err);
@@ -49,14 +52,17 @@ router.get("/user", passport.authenticate("jwt", { session: false }), async func
  * or HTTP 400 and JSON of {success: false, message: "reason for error"}
  */
 router.get("/application", passport.authenticate("jwt", { session: false }), async function (req, res, next) {
-    // TODO - auth verification
+    const requestorID = req.user?.userID as number;
     const { applicationID } = req.query;
     try {
         const parsedApplicationID = validateAndParseStringID(applicationID);
+        const { userID } = await appModel.getAppByID(parsedApplicationID);
+        validateAuthorization(requestorID, userID);
         const rows = await skillModel.getApplicationSkills(parsedApplicationID);
         res.status(200).send(rows);
     } catch (err) {
         if (err instanceof ValidationError) return res.status(400).json({ success: false, message: err.message });
+        if (err instanceof AuthError) return res.status(401).json({ success: false, message: err.message });
         console.error(`Error in getting application skills:`);
         console.error({ applicationID });
         next(err);
@@ -93,14 +99,17 @@ router.post("/", async function (req, res, next) {
  * or HTTP 400 and JSON of {success: false, message: "reason for error"}
  */
 router.post("/user", passport.authenticate("jwt", { session: false }), async function (req, res, next) {
+    const requestorID = req.user?.userID as number;
     const { userID, skillID, rating } = req.body;
     try {
         const skill: Skill = new Skill({ userID, skillID, rating });
         skill.validateAndAssertContains(["userID", "skillID", "rating"]);
+        validateAuthorization(requestorID, skill.fields.userID);
         await skillModel.createUserSkill(skill.fields as UserSkillFields);
         res.status(201).send({ success: true });
     } catch (err) {
         if (err instanceof ValidationError) return res.status(400).json({ success: false, message: err.message });
+        if (err instanceof AuthError) return res.status(401).json({ success: false, message: err.message });
         console.error(`Error in creating new skill:`);
         console.error(req.body);
         next(err);
@@ -115,14 +124,18 @@ router.post("/user", passport.authenticate("jwt", { session: false }), async fun
  * or HTTP 400 and JSON of {success: false, message: "reason for error"}
  */
 router.post("/application", passport.authenticate("jwt", { session: false }), async function (req, res, next) {
+    const requestorID = req.user?.userID as number;
     const { applicationID, skillID, name } = req.body;
     try {
         const skill: Skill = new Skill({ applicationID, skillID, name });
         skill.validateAndAssertContains(["applicationID", "skillID"]);
+        const { userID } = await appModel.getAppByID(skill.fields.applicationID);
+        validateAuthorization(requestorID, userID);
         await skillModel.createApplicationSkill(skill.fields as ApplicationSkillFields);
         res.status(201).send({ success: true, applicationID, skillID, name });
     } catch (err) {
         if (err instanceof ValidationError) return res.status(400).json({ success: false, message: err.message });
+        if (err instanceof AuthError) return res.status(401).json({ success: false, message: err.message });
         console.error(`Error in creating new skill:`);
         console.error({ applicationID, skillID, name });
         next(err);
@@ -137,10 +150,12 @@ router.post("/application", passport.authenticate("jwt", { session: false }), as
  * or HTTP 204 and JSON of {success: false, message: "reason for error"}
  */
 router.patch("/user", passport.authenticate("jwt", { session: false }), async function (req, res, next) {
+    const requestorID = req.user?.userID as number;
     const { userID, skillID, rating } = req.body;
     try {
         const skill: Skill = new Skill({ userID, skillID, rating });
         skill.validateAndAssertContains(["userID", "skillID", "rating"]);
+        validateAuthorization(requestorID, skill.fields.userID);
         const didUpdate = await skillModel.updateUserSkillRating(userID, skillID, rating);
         if (!didUpdate) {
             return res.status(404).json({ success: false, message: "No matching row found for that userID/skillID" });
@@ -148,6 +163,7 @@ router.patch("/user", passport.authenticate("jwt", { session: false }), async fu
         res.status(200).send({ success: true, userID, skillID, rating });
     } catch (err) {
         if (err instanceof ValidationError) return res.status(400).json({ success: false, message: err.message });
+        if (err instanceof AuthError) return res.status(401).json({ success: false, message: err.message });
         console.error(`Error in updating user skill rating:`);
         console.error({ userID, skillID, rating });
         next(err);
