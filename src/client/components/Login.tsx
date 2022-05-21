6ee8b1efc54d38231151579fd7674e20f08c25d7
Jon Baird <jbaird29@gmail.com>
2022-05-21 12:56:05 -0400

    add Login feedback of invalid username or password

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -63,7 +63,12 @@ function Login() {
                     window.location.reload();
                 })
                 .catch((err) => {
+                    const errField = err.response?.data?.field
+                    if(errField) alert(`Invalid ${errField}. Please try again.`)
                     console.log(err);
+                }).finally(() => {
+                    setPassword("");
+                    setUsername("");
                 });
         }
     };
